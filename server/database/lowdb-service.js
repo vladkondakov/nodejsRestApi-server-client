b82b5f1358c99f81b8a5d7ca014872504e9b2c98
Vlad Kondakov <vladisloove.kondakov@gmail.com>
2021-03-15 15:43:17 +0300

    add database and config folders

---
+++ server/database/lowdb-service.js
@@ -0,0 +1,43 @@
+const lowDb = require('lowdb');
+const FileAsync = require('lowdb/adapters/FileSync');
+
+class LowDBOperations {
+    static async connect(dbName) { 
+        return lowDb(new FileAsync(`../server/database/${dbName}.json`));
+    }
+
+    static async getElement(dbName, searchParam) {
+        const db = await this.connect(dbName);
+        const element = db.get(dbName).find(searchParam).value();
+        return element;
+    }
+
+    static async getAllElements(dbName) {
+        const db = await this.connect(dbName);
+        const elements = db.get(dbName).value();
+        return elements;
+    }
+
+    static async saveElement(dbName, element) {
+        const db = await this.connect(dbName);
+        await db.get(dbName).push(element).write();
+    }
+
+    static async deleteElement(dbName, deleteParam) {
+        const db = await this.connect(dbName);
+        const elementToDelete = await this.getElement(dbName, deleteParam);
+        await db.get(dbName).remove(elementToDelete).write();
+    }
+
+    static async pullElement(dbName, element) {
+        const db = await this.connect(dbName);
+        await db.get(dbName).pull(element).write();
+    }
+
+    static async updateElement(dbName, updateParam, element) {
+        const db = await this.connect(dbName);
+        await db.get(dbName).find(updateParam).assign(element).write();
+    }
+}
+
+module.exports = LowDBOperations;
