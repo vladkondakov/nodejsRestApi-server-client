1e99e3035dc6aadb77e1006c55899a76525724f1
Vlad Kondakov <vladisloove.kondakov@gmail.com>
2021-04-27 11:06:44 +0300

    refactor: change surname pattern to agree only letters in schemas

--- server/helpers/validation-schema.js
+++ server/helpers/validation-schema.js
@@ -5,7 +5,7 @@ const signUpSchema = Joi.object().keys({
     password: Joi.string().pattern(/^\w+$/).min(3).max(16).required(),
     confirmationPassword: Joi.any().valid(Joi.ref('password')).required(),
     name: Joi.string().pattern(/^[a-zA-z]+$/).min(1).max(100).allow(''),
-    surname: Joi.string().alphanum().min(1).max(100).allow(''),
+    surname: Joi.string().pattern(/^[a-zA-z]+$/).min(1).max(100).allow(''),
     dateOfBirth: Joi.date().greater('1-1-1940').iso().allow(''),
     position: Joi.array().valid('Junior Software Engineer', 'Software Engineer', 'Junior Backend Developer').allow(''),
     salary: Joi.number().min(0).allow('')
@@ -18,7 +18,7 @@ const loginSchema = Joi.object().keys({
 
 const employeeSchema = Joi.object().keys({
     name: Joi.string().pattern(/^[a-zA-z]+$/).min(1).max(100).allow(''),
-    surname: Joi.string().alphanum().min(1).max(100).allow(''),
+    surname: Joi.string().pattern(/^[a-zA-z]+$/).min(1).max(100).allow(''),
     dateOfBirth: Joi.date().greater('1-1-1940').iso().allow(''),
     position: Joi.array().valid('Junior Software Engineer', 'Software Engineer', 'Junior Backend Developer').allow(''),
     salary: Joi.number().min(0).allow('')
